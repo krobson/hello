5a6f8d0a1fb9fc225fd54a2e4c149b144fc82a58
Ken Robson <ken@robson.net>
2022-08-04 11:40:04 +0200

    Updated message.

--- hello-openshift-for-developers.go
+++ hello-openshift-for-developers.go
@@ -9,7 +9,7 @@ import (
 func helloHandler(w http.ResponseWriter, r *http.Request) {
 	response := os.Getenv("RESPONSE")
 	if len(response) == 0 {
-		response = "Hello OpenShift for Developers!"
+		response = "Hello Sponge Bob Square Pants!"
 	}
 
 	fmt.Fprintln(w, response)
